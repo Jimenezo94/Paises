16fa0c6b60549b0548f2ad4aa2ae54d2f94df48a
Juliana Jimenez <nani.jimenez@hotmail.com>
2022-01-31 12:30:42 -0500

    search

--- script/main.js
+++ script/main.js
@@ -23,8 +23,6 @@ const mostrarDato = async () =>{
 document.addEventListener('DOMContentLoaded', mostrarDato);
 
 //BUSCADOR
-const formulario = document.querySelector('#formulario')
-const resultado = document.querySelector('#resultado')
 
 const res = await fetch(url);
 const dato2= await res.json();
@@ -58,49 +56,3 @@ function autocompletado () {
 
 autocompletado()
 
-autocompletado
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const buscar = async () =>{
-//     const res = await fetch(url);
-//     const dato2= await res.json();
-
-//     console.log(formulario.value)
-//     const buscado = formulario.value.toLowerCase();
-//     for(let pais of dato2){
-//         console.log(dato2)
-//         console.log(pais)
-//         // let palabra = pais.name.toLowerCase()
-//         // if(palabra.indexOf(buscado) !== -1){
-//         //     const {id, name, image, Population, Region, capital} = palabra;
-//         //     card.innerHTML += `
-//         //     <div class="card" style="width: 18rem;">
-//         //                         <img src="${image}" class="card-img-top" alt="">
-//         //                         <div class="card-body">
-//         //                             <h1>${name}</h1>
-//         //                             <h6 class="info">population: ${Population}</h6>
-//         //                             <h6 class="info">region: ${Region}</h6>
-//         //                             <h6 class="info">capital: ${capital}</h6>
-//         //                             <input class="btn btn-primary" type="submit" value="Submit">
-//         //                         </div>
-//         //                     </div>
-//         //     `
-//         //        }
-//     }
-
-// }
-// boton.addEventListener('click', buscar);
