0b83c53860e660b02120fdcda18b5b82739353c9
Juliana Jimenez <nani.jimenez@hotmail.com>
2022-01-31 14:33:35 -0500

    modOscuro

--- script/main.js
+++ script/main.js
@@ -55,4 +55,24 @@ function autocompletado () {
 
 
 autocompletado()
+ 
 
+//MODO OSCURO
+
+ 
+const boton = document.querySelector('#boton');
+const prefresDarkScheme = window.matchMedia('(prefers-color-scheme: dark)')
+
+boton.addEventListener('click', () => {
+  
+    if (prefresDarkScheme.matches) {
+        document.body.classList.toggle('light-theme')
+        document.card.classList.toggle('light-theme')
+
+    } else {
+        document.body.classList.toggle('dark-theme')
+        document.card.classList.toggle('dark-theme')
+
+    }
+
+})
